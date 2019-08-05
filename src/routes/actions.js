e4a33d42527f89e88594740faa075c26b1f87f02
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-08-05 11:57:45 -0700

    complete routes/actions

--- src/routes/actions.js
+++ src/routes/actions.js
@@ -2,3 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const actions = require("../controllers/actions");
+
+router.get("/", actions.all);
+router.get("/:id", actions.one);
+router.post("/", actions.new);
+router.delete("/:id", actions.rm);
+router.put("/:id", actions.update);
